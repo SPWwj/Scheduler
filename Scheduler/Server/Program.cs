e2c53f93b587c0921305cb0720350bcc0edeac5b
WStoneDev <wstonedev@outlook.com>
2022-01-05 07:01:19 +0800

    cors

--- Scheduler/Server/Program.cs
+++ Scheduler/Server/Program.cs
@@ -12,6 +12,16 @@ builder.Services.AddSignalR(conf =>
 {
     conf.MaximumReceiveMessageSize = null;
 });
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+        .AllowAnyHeader()
+        .AllowCredentials()
+        .AllowAnyMethod();
+    });
+});
 //builder.Services.AddSignalR();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
@@ -35,34 +45,35 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.Use(async (context, next) =>
-{
+//app.Use(async (context, next) =>
+//{
 
-    if (context.Request.Method == "OPTIONS")
-    {
+//    if (context.Request.Method == "OPTIONS")
+//    {
 
-        //允许处理跨域
-        context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
-        context.Response.Headers.Add("Access-Control-Allow-Headers", "*");
-        context.Response.Headers.Add("Access-Control-Allow-Methods", "*");
-        await context.Response.CompleteAsync();
-    }
-    else
-    {
+//        //允许处理跨域
+//        context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+//        context.Response.Headers.Add("Access-Control-Allow-Headers", "*");
+//        context.Response.Headers.Add("Access-Control-Allow-Methods", "*");
+//        await context.Response.CompleteAsync();
+//    }
+//    else
+//    {
 
-        //允许处理跨域
-        context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
-        context.Response.Headers.Add("Access-Control-Allow-Headers", "*");
-        context.Response.Headers.Add("Access-Control-Allow-Methods", "*");
-        await next();
-    }
-});
+//        //允许处理跨域
+//        context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+//        context.Response.Headers.Add("Access-Control-Allow-Headers", "*");
+//        context.Response.Headers.Add("Access-Control-Allow-Methods", "*");
+//        await next();
+//    }
+//});
 app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseCors();
 
 app.MapRazorPages();
 app.MapControllers();
